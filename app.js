b9865e66913fcc4b8d8bdb84ed4e2319f863bef2
Simao Gama <gama.simao@gmail.com>
2023-07-11 16:07:48 +0100

    changes

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const express = require('express');
 
 const app = express();
 
+const { isAuthenticated } = require('./middleware/jwt.middleware');
+
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require('./config')(app);
 
