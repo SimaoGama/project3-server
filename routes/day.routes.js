92f7f6ee8323010afe6f2876e49de7214b64f683
Simao Gama <gama.simao@gmail.com>
2023-07-23 19:12:21 +0100

    changes

--- routes/day.routes.js
+++ routes/day.routes.js
@@ -1,39 +1,42 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
-const StatusCodes = require('http-status-codes').StatusCodes;
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const StatusCodes = require("http-status-codes").StatusCodes;
 // const fileUploader = require('../config/cloudinary.config');
 
 //importing the models
-const User = require('../models/User.model');
-const Trip = require('../models/trip-models/Trip.model');
-const Day = require('../models/trip-models/Day.model');
-const Accommodation = require('../models/trip-models/Accommodation.model');
-const Restaurant = require('../models/trip-models/Restaurant.model');
-const Plan = require('../models/trip-models/Plan.model');
-const City = require('../models/trip-models/City.model');
-const Location = require('../models/trip-models/Location.model');
+const User = require("../models/User.model");
+const Trip = require("../models/trip-models/Trip.model");
+const Day = require("../models/trip-models/Day.model");
+const Accommodation = require("../models/trip-models/Accommodation.model");
+const Restaurant = require("../models/trip-models/Restaurant.model");
+const Plan = require("../models/trip-models/Plan.model");
+const City = require("../models/trip-models/City.model");
+const Location = require("../models/trip-models/Location.model");
 
-router.put('/:dayId', async (req, res) => {
+router.put("/:dayId", async (req, res) => {
   const { dayId } = req.params;
   const selectedPlace = req.body.selectedPlace;
+
+  console.log("dayID", dayId);
+  // console.log("selectedPlace", selectedPlace);
 
   try {
     // Find the day by ID
     const day = await Day.findById(dayId);
 
     if (!day) {
-      return res.status(StatusCodes.NOT_FOUND).json({ error: 'Day not found' });
+      return res.status(StatusCodes.NOT_FOUND).json({ error: "Day not found" });
     }
 
     const newLocation = new Location({
       lat: selectedPlace.latitude,
-      lng: selectedPlace.longitude
+      lng: selectedPlace.longitude,
     });
 
     const createdLocation = await newLocation.save();
 
     // Check the type of the selected place
-    if ('price_level' in selectedPlace && 'cuisine' in selectedPlace) {
+    if ("price_level" in selectedPlace && "cuisine" in selectedPlace) {
       const newRestaurant = await Restaurant.create({
         name: selectedPlace.name,
         location: createdLocation._id,
@@ -41,17 +44,19 @@ router.put('/:dayId', async (req, res) => {
         phone: selectedPlace.phone,
         rating: selectedPlace.rating,
         reviews: selectedPlace.reviews,
-        photos: selectedPlace.photos
+        photos: selectedPlace.photos,
       });
 
       await Day.findByIdAndUpdate(day._id, {
         $push: {
-          restaurants: newRestaurant._id
-        }
+          restaurants: newRestaurant._id,
+        },
       });
+      console.log("newRestaurantID:", newRestaurant._id);
+      console.log("updatedRestaurant:", newRestaurant);
     } else if (
-      'hotel_class' in selectedPlace &&
-      'special_offers' in selectedPlace
+      "hotel_class" in selectedPlace &&
+      "special_offers" in selectedPlace
     ) {
       const newAccommodation = await Accommodation.create({
         name: selectedPlace.name,
@@ -59,69 +64,81 @@ router.put('/:dayId', async (req, res) => {
         classification: selectedPlace.hotel_class,
         rating: selectedPlace.rating,
         reviews: selectedPlace.reviews,
-        photos: selectedPlace.photos
+        photos: selectedPlace.photos,
       });
 
       await Day.findByIdAndUpdate(day._id, {
-        accommodation: newAccommodation._id
+        accommodation: newAccommodation._id,
       });
     } else {
       // Handle other types if needed
       return res
         .status(StatusCodes.BAD_REQUEST)
-        .json({ error: 'Invalid place type' });
+        .json({ error: "Invalid place type" });
     }
 
     // Save the updated day
     const updatedDay = await day.save();
-    console.log(updatedDay);
+    console.log("BE updatedDay:", updatedDay);
 
     res.json(updatedDay);
   } catch (error) {
-    console.error('Error updating day:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    if (error.message === "Invalid place type") {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Invalid place type" });
+    }
+    console.error("Error updating day:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
 // GET restaurant by ID
-router.get('/restaurant/:restaurantId', async (req, res) => {
+router.get("/restaurant/:restaurantId", async (req, res) => {
   const { restaurantId } = req.params;
 
   try {
     const restaurant = await Restaurant.findById(restaurantId);
     res.json(restaurant);
   } catch (err) {
-    res.status(500).json({ error: 'Unable to fetch restaurant data.' });
+    res.status(500).json({ error: "Unable to fetch restaurant data." });
   }
 });
 
 // GET accommodation by ID
-router.get('/accommodation/:accommodationId', async (req, res) => {
+router.get("/accommodation/:accommodationId", async (req, res) => {
   const { accommodationId } = req.params;
 
   try {
     const accommodation = await Accommodation.findById(accommodationId);
     res.json(accommodation);
   } catch (err) {
-    res.status(500).json({ error: 'Unable to fetch accommodation data.' });
+    res.status(500).json({ error: "Unable to fetch accommodation data." });
   }
 });
 
-router.delete('/restaurant/:restaurantId', async (req, res) => {
+router.delete("/restaurant/:restaurantId", async (req, res) => {
   const { restaurantId } = req.params;
 
   try {
     const deletedRestaurant = await Restaurant.findByIdAndDelete(restaurantId);
     if (!deletedRestaurant) {
-      return res.status(404).json({ error: 'Restaurant not found.' });
+      return res.status(404).json({ error: "Restaurant not found." });
     }
-    res.json({ message: 'Restaurant deleted successfully.' });
+
+    // Remove the restaurant from all days where it is referenced
+    await Day.updateMany(
+      { restaurants: restaurantId },
+      { $pull: { restaurants: restaurantId } }
+    );
+
+    res.json({ message: "Restaurant deleted successfully." });
   } catch (err) {
-    res.status(500).json({ error: 'Unable to delete restaurant.' });
+    res.status(500).json({ error: "Unable to delete restaurant." });
   }
 });
 
-router.delete('/accommodation/:accommodationId', async (req, res) => {
+router.delete("/accommodation/:accommodationId", async (req, res) => {
   const { accommodationId } = req.params;
 
   try {
@@ -129,11 +146,18 @@ router.delete('/accommodation/:accommodationId', async (req, res) => {
       accommodationId
     );
     if (!deletedAccommodation) {
-      return res.status(404).json({ error: 'Accommodation not found.' });
+      return res.status(404).json({ error: "Accommodation not found." });
     }
-    res.json({ message: 'Accommodation deleted successfully.' });
+
+    // Remove the accommodation from all days where it is referenced
+    await Day.updateMany(
+      { accommodation: accommodationId },
+      { $unset: { accommodation: 1 } }
+    );
+
+    res.json({ message: "Accommodation deleted successfully." });
   } catch (err) {
-    res.status(500).json({ error: 'Unable to delete accommodation.' });
+    res.status(500).json({ error: "Unable to delete accommodation." });
   }
 });
 
